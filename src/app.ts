94c50f8a6760dead32705bb5dcec09e5153ef67b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-01-12 14:44:46 +0300

    code cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 // App styles
 import '@styles/app.scss';
-import { helloButtonFactory } from '@/factories/HelloButtonFactory';
 
 // Bootstrap
 // Uncomment lines below to use Bootstrap
@@ -9,6 +8,3 @@ import { helloButtonFactory } from '@/factories/HelloButtonFactory';
 // JQuery
 // Uncomment lines import bellow to use JQuery
 // import $ from 'jquery';
-
-// Factories
-helloButtonFactory;
